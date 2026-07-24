b42a4deb02550a9dc12d3bbb2b73f5f548d59d18
grupocaetanodigital-creator <grupocaetanodigital@gmail.com>
2026-07-24 00:12:52 -0300

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,6 @@
-// Caminho: js/supabaseClient.js
-const SUPABASE_URL = 'https://uxayfqeqqcgpwysxnfjw.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4YXlmcWVxcWNncHd5c3huZmp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0ODByRmA0ODprRmA4bm9vYjJBNmlpLWJmU1BBWEEyS2cgU0M';
-
-export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+fetch('https://uxayfqeqqcgpwysxnfjw.supabase.co/rest/v1/condominios?select=*', {
+  headers: {
+    'apikey': eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4YXlmcWVxcWNncHd5c3huZmp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4NDU5MjYsImV4cCI6MjEwMDQyMTkyNn0.edwTbjwfNb48dprFmARnoobJjBNii-bfSPAXAO2sgO8
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4YXlmcWVxcWNncHd5c3huZmp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4NDU5MjYsImV4cCI6MjEwMDQyMTkyNn0.edwTbjwfNb48dprFmARnoobJjBNii-bfSPAXAO2sgO8
+  }
+}).then(res => res.json()).then(console.log);
