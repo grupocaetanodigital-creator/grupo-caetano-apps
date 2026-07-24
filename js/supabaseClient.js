2e32ab629e845203243f3c3a0dfd6359259dcd72
grupocaetanodigital-creator <grupocaetanodigital@gmail.com>
2026-07-24 00:04:23 -0300

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
 // Caminho: js/supabaseClient.js
 const SUPABASE_URL = 'https://uxayfqeqqcgpwysxnfjw.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_4MV0hTSJTXaez9YSdAnu6A_T-UdhySi';
+const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4YXlmcWVxcWNncHd5c3huZmp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4NDU5MjYsImV4cCI6MjEwMDQyMTkyNn0.edwTbjwfNb48dprFmARnoobJjBNii-bfSPAXAO2sgO8
 
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
