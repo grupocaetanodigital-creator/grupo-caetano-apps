b1d7be75d6dbbfee4a621d6099a4912d489afb30
grupocaetanodigital-creator <grupocaetanodigital@gmail.com>
2026-07-23 21:22:19 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Caminho: sw.js
+const CACHE_NAME = 'caetano-apps-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './css/custom.css'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
