1ccac049da19efd13cac11fa865b73aa2f3d5880
grupocaetanodigital-creator <grupocaetanodigital@gmail.com>
2026-07-23 21:26:17 -0300

    Create supabaseClient.js

---
+++ css/js/supabaseClient.js
@@ -0,0 +1,5 @@
+// Caminho: js/supabaseClient.js
+const SUPABASE_URL = https://uxayfqeqqcgpwysxnfjw.supabase.co/rest/v1/
+const SUPABASE_ANON_KEY = sb_publishable_4MV0hTSJTxaez9YSdAnu6A_T-UdhySi
+
+export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
